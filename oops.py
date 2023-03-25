07a6c357d9a686cb2fc831bf3b0b07c4d5da0257
Ramuswathi <126935437+Ramuswathi@users.noreply.github.com>
2023-03-25 14:01:15 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,10 @@
+class student:
+    def __init__(self,name,age,schoolname):
+        self.name = name
+        self.age = age
+        self.schoolname = school
+
+
+s1 = student('Ramu',32,'abc school')
+print('Student',s1.name,s1.age)
+print('School name',s1.schoolname)
